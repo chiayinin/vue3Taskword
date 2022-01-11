7dbcf2ac45f52d8fb75ae870a29ec6473e5670c6
chiayinin <chiayinin@gmail.com>
2022-01-11 16:08:49 +0800

    htmlDone

--- all.js
+++ all.js
@@ -55,6 +55,7 @@ let products=[
 // ESM
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
 
+
 const app = createApp({
   data(){
     const message="Hello world!";
