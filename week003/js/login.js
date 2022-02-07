f17c329e3533f9a848ab11e74282a7310ab1e250
chiayinin <chiayinin@gmail.com>
2022-02-07 20:54:34 +0800

    loginDone

---
+++ week003/js/login.js
@@ -0,0 +1,54 @@
+// ESM
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
+
+const app=createApp({
+  data(){
+    return{
+      // 加入API站點，個人API path
+      apiUrl:'https://vue3-course-api.hexschool.io/v2',
+      apiPath:'chiayinin-api',
+      user:{
+        username:'',
+        password:''
+      }
+    };
+  },
+
+  methods:{
+    // 登入
+    login(){
+      // 將資料發送到遠端並登入、將Token儲存在cookie裡面
+      const url=`${this.apiUrl}/admin/signin`;
+      const user=this.user;
+
+      axios.post(url, user)
+      // 成功
+      .then(response=>{
+        // 取出res內的Token值,到期值
+        const {token, expired}=response.data;
+        // 讀取Token
+        console.log(token, expired);
+        document.cookie = `chiayininApi=${token}; expires=${new Date(expired)};`;
+
+        // 將Token儲存在cookie
+        // 取得Token
+        var myToken = document.cookie.replace(/(?:(?:^|.*;\s*)chiayininApi\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        // Token的內容加到headers裡面成為每次axios發送後的預設值
+        axios.defaults.headers.common['Authorization'] = myToken;
+
+        // 登入成功後立刻轉址
+        window.location.href = 'products.html'
+        // this.$router.push('/products.html')
+      })
+      .catch(error=>{
+        console.dir(error)
+        alert("登入失敗，請重新檢查帳號、密碼是否正確。")
+      })
+    }
+
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  app.mount('#app');
+})
---
+++ week003/js/products.js
@@ -0,0 +1,9 @@
+import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
+
+const app=createApp({
+
+});
+
+window.addEventListener('DOMContentLoaded', function(){
+  app.mount('#app');
+})
