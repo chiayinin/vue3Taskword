13f35e547c7326c6d372a1ebe312b5e7bc3f9d84
chiayinin <chiayinin@gmail.com>
2022-03-11 11:22:12 +0800

    addProductList

--- week006/src/router/index.js
+++ week006/src/router/index.js
@@ -4,23 +4,43 @@ import HomeView from "../views/HomeView.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    component: () => import("../views/FrontView.vue"),
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "product",
+        component: () => import("../views/ProductView.vue"),
+      },
+      {
+        path: "cart",
+        component: () => import("../views/CartView.vue"),
+      },
+    ],
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/admin",
+    component: () => import("../views/DashboardView.vue"),
+    children: [
+      {
+        path: "product",
+        component: () => import("../views/AdminProductView.vue"),
+      },
+      {
+        path: "coupon",
+        component: () => import("../views/AdminCouponView.vue"),
+      },
+    ],
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  linkActiveClass: "active",
 });
 
 export default router;
---
+++ week006/src/views/ProductView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>產品列表</h2>
+  {{ product }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      product: [],
+    };
+  },
+  methods: {
+    getProduct() {
+      this.$http(
+        `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products/all`
+      )
+        .then((response) => {
+          this.product = response.data.products;
+        })
+        .catch((error) => {
+          console.dir(error);
+        });
+    },
+  },
+  mounted() {
+    this.getProduct();
+  },
+};
+</script>
